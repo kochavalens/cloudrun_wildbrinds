9bdd1cbfdd981c93ea9503c88b30e3ef5ea68409
victorvalenzuelakochasoft <victor.valenzuela@kochasoft.com>
2022-08-08 11:57:07 -0400

    Add new endpoint

--- src/main/java/com/ms/consume/api/controller/ConsumeAPIController.java
+++ src/main/java/com/ms/consume/api/controller/ConsumeAPIController.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -69,6 +70,13 @@ public class ConsumeAPIController {
 		
 		String response = "TEST";
 		return response;
+
+	}
+	
+	@PostMapping("/consumeApis")
+	public String consumeAPIS(@RequestBody String request) {
+		
+		return request;
 		
 	}
 
