4eabbd52a70db37a4b68da3807e09fb514147582
victorvalenzuelakochasoft <victor.valenzuela@kochasoft.com>
2022-08-14 01:04:20 -0400

    Ajustes swagger

--- src/main/java/com/ms/consume/api/MsConsumeApiApplication.java
+++ src/main/java/com/ms/consume/api/MsConsumeApiApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class MsConsumeApiApplication {
 
